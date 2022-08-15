f13dc8729b838c175fc7aca6f68ab3c7fc6f9edf
VozunMaxim <vozun.maxim@gmail.com>
2022-08-15 20:24:40 +0300

    third commit

---
+++ src/main/java/ru/netology/Dz_9_Java_oop/Radio.java
@@ -0,0 +1,75 @@
+package ru.netology.Dz_9_Java_oop;
+
+public class Radio {
+
+    private int currentStation; // ПАРАМЕТРЫ СТАНЦИЙ
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void setToMaxStation() {
+        currentStation = 9;
+    }
+
+
+    public void StationUp() {  //цикличное повышение
+        if (currentStation <= 9)
+            currentStation++;
+
+        if (currentStation > 9)
+            currentStation = 0;
+
+    }
+
+    public void StationDown() {   //цикличное понижение
+        if (currentStation <= 9)
+            currentStation--;
+
+        if (currentStation < 0)
+            currentStation = 9;
+
+    }
+
+    private int currentVolume;  // ПАРАМЕТРЫ ГРОМКОСТИ
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 10;
+    }
+
+    public void volumeUp() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void volumeDown() {
+        if (currentVolume <= 10) {
+            currentVolume--;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/Dz_9_Java_oop/RadioTest.java
@@ -0,0 +1,123 @@
+package ru.netology.Dz_9_Java_oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test                                        //ТЕСТЫ ПЕРЕКЛЮЧЕНИЯ СТАНЦИЙ
+    public void shouldSetStation() {
+        Radio cond = new Radio();
+        cond.setCurrentStation(9);
+
+        int expected = 9;
+        int actual = cond.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetStationAboveMax() {
+        Radio cond = new Radio();
+        cond.setCurrentStation(50);
+
+        int expected = 0;
+        int actual = cond.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //
+    public void shouldSetToMaxStation() {
+        Radio cond = new Radio();
+
+        cond.setToMaxStation();
+
+        int expected = 9;
+        int actual = cond.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //
+    public void shouldStationUp() {
+        Radio cond = new Radio();
+        cond.setCurrentStation(9);
+        cond.StationUp();
+
+        int expected = 0;
+        int actual = cond.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //
+    public void shouldStationDown() {
+        Radio cond = new Radio();
+        cond.setCurrentStation(0);
+        cond.StationDown();
+
+        int expected = 9;
+        int actual = cond.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test                                    //ТЕСТЫ ПЕРЕКЛЮЧЕНИЯ ГРОМКОСТИ
+    public void shouldSetVolume() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(10);
+
+        int expected = 10;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //
+    public void shouldNotSetVolumeAboveMax() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(15);
+
+        int expected = 0;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetToMaxVolume() {
+        Radio cond = new Radio();
+
+        cond.setToMaxVolume();
+
+        int expected = 10;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //
+    public void shouldVolumeUp() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(7);
+        cond.volumeUp();
+
+        int expected = 8;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //
+    public void shouldVolumeDown() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(10);
+        cond.volumeDown();
+
+        int expected = 9;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
