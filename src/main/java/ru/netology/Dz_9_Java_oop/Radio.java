a82975f3106bb64a609e7af84da4455e94d57374
VozunMaxim <vozun.maxim@gmail.com>
2022-08-23 20:11:45 +0300

    Dz 10 for Pull Request

--- src/main/java/ru/netology/Dz_9_Java_oop/Radio.java
+++ src/main/java/ru/netology/Dz_9_Java_oop/Radio.java
@@ -1,74 +1,101 @@
 package ru.netology.Dz_9_Java_oop;
 
 public class Radio {
+    private int maxStation;
+    private int minStation;
+    private int currentStation;
+    private int maxVolume;
+    private int minVolume;
+    private int currentVolume;
+    private int numberOfStations;
 
-    private int currentStation; // ПАРАМЕТРЫ СТАНЦИЙ
+    public Radio() {
+        numberOfStations = 10;
+        maxStation = 9;
+        minStation = 0;
+        currentStation = minStation;
+        maxVolume = 100;
+        minVolume = 0;
+        currentVolume = minVolume;
+    }
 
-    public int getCurrentStation() {
+    public int numberOfStations() {
+        return numberOfStations;
+    }
+
+    public int getCurrentStation() {     // ПАРАМЕТРЫ СТАНЦИЙ
         return currentStation;
     }
 
     public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+        if (newCurrentStation < minStation) {
             return;
         }
-        if (newCurrentStation > 9) {
+        if (newCurrentStation > maxStation) {
             return;
         }
         currentStation = newCurrentStation;
     }
 
     public void setToMaxStation() {
-        currentStation = 9;
+        currentStation = maxStation;
     }
 
-
     public void StationUp() {  //цикличное повышение
-        if (currentStation <= 9) {
-            currentStation ++;
-        }
-        if (currentStation > 9) {
-            currentStation = 0;
+        if (currentStation >= maxStation) {
+            currentStation = minStation;
+        } else {
+            currentStation++;
         }
     }
 
     public void StationDown() {   //цикличное понижение
-        if (currentStation <= 9)
+        if (currentStation <= minStation) {
+            currentStation = maxStation;
+            return;
+        }
+        if (currentStation >= maxStation) {
+            currentStation = maxStation;
+        } else {
             currentStation--;
-
-        if (currentStation < 0)
-            currentStation = 9;
-
+        }
     }
 
-    private int currentVolume;  // ПАРАМЕТРЫ ГРОМКОСТИ
-
-    public int getCurrentVolume() {
+    public int getCurrentVolume() {     // ПАРАМЕТРЫ ГРОМКОСТИ
         return currentVolume;
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0) {
+        if (newCurrentVolume < minVolume) {
             return;
         }
-        if (newCurrentVolume > 10) {
+        if (newCurrentVolume > maxVolume) {
             return;
         }
         currentVolume = newCurrentVolume;
     }
 
     public void setToMaxVolume() {
-        currentVolume = 10;
+        currentVolume = maxVolume;
     }
 
     public void volumeUp() {
-        if (currentVolume < 10) {
+        if (currentVolume >= maxVolume) {
+            currentVolume = maxVolume;
+        } else {
             currentVolume++;
         }
     }
 
     public void volumeDown() {
-        if (currentVolume <= 10) {
+
+        if (currentVolume <= minVolume) {
+            currentVolume = minVolume;
+            return;
+        }
+        if (currentVolume >= maxVolume) {
+            currentVolume = maxVolume;
+        } else {
             currentVolume--;
         }
     }
